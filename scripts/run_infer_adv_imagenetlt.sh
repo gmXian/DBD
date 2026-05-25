bdd9e0863d8385764767378dd6dabebdca8dd6e0
贤冠萌 <73027349+gmXian@users.noreply.github.com>
2026-05-25 14:48:36 +0800

    Include attack steps in adversarial inference paths

--- scripts/run_infer_adv_imagenetlt.sh
+++ scripts/run_infer_adv_imagenetlt.sh
@@ -6,12 +6,13 @@ ARCH=${ARCH:-ViT-B/32}
 LT_SPLIT=${LT_SPLIT:-val}
 ATTACK=${ATTACK:-pgd}
 EPS=${EPS:-4.0}
+STEPS=${STEPS:-100}
 SEED=${SEED:-0}
 NUM_WORKERS=${NUM_WORKERS:-8}
 OUT_DIR=${OUT_DIR:-outputs/imagenetlt}
 ADV_ROOT=${ADV_ROOT:-./adv_images}
 ARCH_TAG=${ARCH//\//-}
-ADV_DIR=${ADV_DIR:-${ADV_ROOT}/${ARCH_TAG}_${ATTACK}_eps${EPS}_${LT_SPLIT}}
+ADV_DIR=${ADV_DIR:-${ADV_ROOT}/${ARCH_TAG}_${ATTACK}_steps${STEPS}_eps${EPS}_${LT_SPLIT}}
 
 mkdir -p "${OUT_DIR}"
 
@@ -23,4 +24,4 @@ python infer_dbd.py \
   --arch "${ARCH}" \
   -n "${NUM_WORKERS}" \
   --seed "${SEED}" \
-  --classwise_csv "${OUT_DIR}/adv_${LT_SPLIT}_${ARCH_TAG}_${ATTACK}_eps${EPS}_classwise.csv"
+  --classwise_csv "${OUT_DIR}/adv_${LT_SPLIT}_${ARCH_TAG}_${ATTACK}_steps${STEPS}_eps${EPS}_classwise.csv"
